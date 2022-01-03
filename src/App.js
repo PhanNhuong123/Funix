8f7d605995a7135d74bf04e3bd8fb552205161db
PhanNhuong123 <92664134+PhanNhuong123@users.noreply.github.com>
2022-01-03 14:13:51 +0700

    css todo

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import {STAFFS} from './staffs.jsx';
 import dateFormat, { masks } from "dateformat";
 import {BrInfo} from './background'
 
-const btnLayOut = document.getElementsByClassName('layoutBtn')[0];
-console.log(btnLayOut)
-
 
 function App() {
-  const ramdomBr = array => Math.floor(Math.random()*array.length)
+  const ramdomBr = array =>
+    Math.floor(Math.random()*array.length)
   const [infoStaff, setInfostaff] = useState('');
+
   const [BrImg, setBrImg] = useState(BrInfo[ramdomBr(BrInfo)])
+
+  const handleClickBr= () => setBrImg(BrInfo[ramdomBr(BrInfo)])
+
   const handleClickLayout = () => {
    let staffs = document.getElementsByClassName('staff-name');
-   console.log(staffs)
    for (const staff of staffs) {
       if(staff.classList.contains('col-4')){
         staff.classList.remove('col-4')
@@ -29,12 +30,16 @@ function App() {
 
   }
   const handleClick = props => {
-  setBrImg(BrInfo[ramdomBr(BrInfo)])
+  
   setInfostaff(
     <div style={BrImg} className='info col-10'  >
       <h1>Họ và tên: {props.name}</h1>
-       <p>Ngày sinh: {dateFormat(props.doB,"mm/dd/yyyy")}</p>
-       <p>Ngày vào công ty: {dateFormat(props.startDate,"mm/dd/yyyy")}</p>
+       <p>Ngày sinh:
+          {dateFormat(props.doB,"mm/dd/yyyy")}
+       </p>
+       <p>Ngày vào công ty: 
+         {dateFormat(props.startDate,"mm/dd/yyyy")}
+       </p>
        <p>Phòng ban: {props.department.name}</p>
        <p>Số ngày nghỉ còn lại: {props.annualLeave}</p>
        <p>Số ngày đã làm thêm: {props.overTime}</p>
@@ -46,7 +51,16 @@ function App() {
     <div className='wrapper'>
       <div className='header' >
         <h1>Ứng dụng quản lý nhân sự v1.0</h1>
-        <button onClick={handleClickLayout} className='layoutBtn'>Layout</button>
+        <button onClick=
+        {handleClickLayout} className='layoutBtn'
+        >
+          Layout
+        </button>
+        <button onClick=
+        {handleClickBr} className='layoutBtn'
+        >
+        Style
+        </button>
       </div>
       <div className='body row' >
         <ul>
--- src/component/staffListComponent.js
+++ src/component/staffListComponent.js
@@ -1,7 +1,7 @@
 function StaFFList({props, onClick , br}){
     
 return(
-    <li className="staff-name col-4">
+    <li className="staff-name col-4 col col-1">
         <button style={br} onClick={() => onClick(props)
                 
         }>
