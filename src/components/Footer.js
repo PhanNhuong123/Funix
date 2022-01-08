3f3ea242efa9a7c499ec257f41d529b4a457dfdd
PhanNhuong123 <92664134+PhanNhuong123@users.noreply.github.com>
2022-01-08 18:16:19 +0700

    footer simple

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,22 @@
 function Footer() {
-return (
-    <h1>footer</h1>
-)
-};
-export default Footer
-
+  return (
+    <div className="footer">
+      <ul className="footer__address">
+        <h4>Our Address</h4>
+        <li>121,Clear Water Bay Road Clear Water Bay, Kowloon HONG KONG</li>
+        <li>
+          <i class="fas fa-phone-alt"></i>+852 1234 5678
+        </li>
+        <li>
+          <i class="fas fa-blender-phone">+852 2348 4568</i>
+        </li>
+        <li>
+          <i class="fas fa-envelope"></i>
+          <a  href="mailto:www.nhuong.zzz@gmail.com">www.nhuong.zzz@gmail.com</a>
+        </li>
+      </ul>
+      <div className="footer__social"></div>
+    </div>
+  );
+}
+export default Footer;
