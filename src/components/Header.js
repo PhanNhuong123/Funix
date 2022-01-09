ce507117de8a794164f2bec9f1046f55fbf56ada
PhanNhuong123 <92664134+PhanNhuong123@users.noreply.github.com>
2022-01-09 11:10:01 +0700

    staff page todo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,19 @@ function Header() {
   return (
     <div className="header row">
       <ul className="header__nav">
-        <li className="header__nav__entry col-2">
+        <li className="header__nav__entry js_staffs  js_non-active js_active col-2">
           <span>
             <i className="fas fa-users"></i>
           </span>
           <h3>Nhân viên</h3>
         </li>
-        <li className="header__nav__entry col-2">
+        <li className="header__nav__entry js_departments js_non-active col-2">
           <span>
             <i className="fas fa-id-card"></i>
           </span>
           <h3>Phòng Ban</h3>
         </li>
-        <li className="header__nav__entry col-2">
+        <li className="header__nav__entry js_salary  js_non-active col-2">
           <span>
             <i className="fas fa-money-check-alt"></i>
           </span>
