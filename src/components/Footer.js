a3964b50aba1193f942e4ad7b892541171a7da16
PhanNhuong123 <92664134+PhanNhuong123@users.noreply.github.com>
2022-01-09 10:52:54 +0700

    footer todo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="footer">
-      <ul className="footer__address">
+      <ul className="footer__address col-3">
         <h4>Our Address</h4>
         <li>121,Clear Water Bay Road Clear Water Bay, Kowloon HONG KONG</li>
         <li>
@@ -12,10 +12,17 @@ function Footer() {
         </li>
         <li>
           <i class="fas fa-envelope"></i>
-          <a  href="mailto:www.nhuong.zzz@gmail.com">www.nhuong.zzz@gmail.com</a>
+          <a href="mailto:www.nhuong.zzz@gmail.com">www.nhuong.zzz@gmail.com</a>
         </li>
       </ul>
-      <div className="footer__social"></div>
+      <div className="footer__social col-3">
+        <i class="fab fa-google-plus-g footer__social__icon"></i>
+        <i class="fab fa-facebook footer__social__icon"></i>
+        <i class="fab fa-linkedin-in footer__social__icon"></i>
+        <i class="fab fa-twitter footer__social__icon"></i>
+        <i class="fab fa-youtube footer__social__icon"></i>
+        <i class="far fa-envelope footer__social__icon"></i>
+      </div>
     </div>
   );
 }
