5e7fac9f12726ec3811f008157b0511e559fe10f
PhanNhuong123 <92664134+PhanNhuong123@users.noreply.github.com>
2022-01-13 16:36:12 +0700

    add comment to salaryPage

--- src/components/pages/SalaryPage.js
+++ src/components/pages/SalaryPage.js
@@ -15,6 +15,7 @@ const SalaryInfo = ({ staff, salary }) => (
 );
 function Salary() {
   const [staffs, setStaffs] = useState(STAFFS);
+  //hàm sắp xếp dữ liệu theo điều kiện của người dùng 
   const SortData = () => {
     let sortResult = [...STAFFS];
 
@@ -79,8 +80,10 @@ function Salary() {
 
     setStaffs(sortResult);
   };
+//hết
 
-  
+// hàm lắng nghe và lấy điều kiện sắp xếp khi người dùng 
+//click thay đổi điều kiện sau đó gọi hàm sắp xếp dữ liêu
   const handleClickSort = () => {
     const icon = document.getElementsByClassName("sort__selec-sort")[0];
     if (icon.innerHTML === '<i class="fas fa-sort-alpha-up-alt"></i>') {
@@ -102,6 +105,7 @@ function Salary() {
       SortData();
     }
   };
+  // hết 
   const basicSalary = 3000000;
   const overTimeSalary = 200000;
   const salaryVal = (overTime, salaryScale) =>
