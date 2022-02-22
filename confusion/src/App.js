6816485ab70773d4f4b1249a94403c2bba252126
PhanNhuong123 <92664134+PhanNhuong123@users.noreply.github.com>
2022-02-22 22:21:04 +0700

    component part 2

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import 'bootstrap'
+import Menu from "./components/MenuComponent";
+import { useState } from "react";
+import { DISHES } from "./share/dishes";
 
 function App() {
+  const [dishes, setDishes] = useState(DISHES);
+  console.log(dishes);
   return (
     <div className="App">
-      <Navbar dark color='primary' > 
-        <div className='container'> 
-          <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-          <Menu/>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu dishes={dishes} />
     </div>
   );
 }
