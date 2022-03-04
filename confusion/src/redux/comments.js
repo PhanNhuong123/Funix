090a39c5460e1700d0b9a317d8eb5029459db28c
Nhuong <www.nhuong.zzz@gmail.com>
2022-03-04 09:51:32 +0700

    Combining Reducer

---
+++ confusion/src/redux/comments.js
@@ -0,0 +1,11 @@
+import { COMMENTS } from "../share/comments";
+
+
+
+
+export const Comments = ( state = COMMENTS, action) => {
+    switch (action.type){
+        default :
+            return state
+    }
+};
