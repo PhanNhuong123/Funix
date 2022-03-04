0ff7cfa39e440e0ce06e86cae819a9b64f2f4cab
Nhuong <www.nhuong.zzz@gmail.com>
2022-03-04 13:24:26 +0700

    Combining Reducers

--- confusion/src/redux/configureStore.js
+++ confusion/src/redux/configureStore.js
@@ -1,8 +1,8 @@
-import {createStore, combineReducers} from 'redux';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
+import {createStore, combineReducers }from 'redux'
 
 export const ConfigureStore = () => {
     const store = createStore(
